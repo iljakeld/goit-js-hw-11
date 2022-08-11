a5a094d34626f391fd092685e57ac526301f46a6
Ilja Keldanovich <ilja.keld@gmail.com>
2022-08-11 21:25:22 +0200

    working on infinite scroll

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -5,11 +5,48 @@ export default apiService
 
 axios.defaults.baseURL = 'https://pixabay.com/api/'
 const API_KEY = '29185882-2e6657626f951957c100977da'
-
-async function apiService(query, page, perPage) {
- // try {}
-    const response = await axios.get(
-      `?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
-    )
-    return response
+export default class ServiceAPI {
+  constructor() {
+    this.options = {
+      params: {
+        key: '29185882-2e6657626f951957c100977da',
+        q: '',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: 1,
+        per_page: 40,
+      },
+    };
   }
+
+  async getPictures() {
+    const response = await axios.get(axios.defaults.baseURL, this.options);
+    this.incrementPage();
+    return response;
+  }
+
+  incrementPage() {
+    this.options.params.page += 1;
+  }
+
+  resetPage() {
+    this.pageNumber = 1;
+  }
+
+  get searchQuery() {
+    return this.options.params.q;
+  }
+
+  set searchQuery(newQuery) {
+    this.options.params.q = newQuery;
+  }
+
+  get pageNumber() {
+    return this.options.params.page;
+  }
+
+  set pageNumber(newNumber) {
+    this.options.params.page = newNumber;
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,12 @@
 import { Notify } from 'notiflix';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import createCollection from './createCollection';
-import apiService from './apiService'; 
-import { scroll } from './scroll'; 
 
-const refs = {
-  searchForm: document.querySelector('#search-form'), 
-  gallery: document.querySelector('.gallery'),
-};
+import refs from './refs'
+import createCollection from './createCollection';
+import ServiceAPI from './apiService'
+import { scroll } from './scroll'; 
 
 let query = ''
 let page = 1
@@ -27,39 +25,5 @@ function onSearchForm(event) {
   if (!query) {
     Notify.failure('The search string cannot be empty. Please specify your search query.')
     return
-  }
-
-  apiService(query, page, perPage)
-    .then(({ data }) => {
-      if (data.totalHits === 0) {
-        Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-      } else {
-        createCollection(data.hits)
-        simpleLightBox.refresh()
-        Notify.success(`Hooray! We found ${data.totalHits} images.`)
-      }
-    })
-    
-    .catch(error => console.log(error))
-  
+  }  
 }
-
-// function onBtnLoad() {
-//   page += 1
-//   //simpleLightBox.destroy()
-
-//   apiService(query, page, perPage)
-//     .then(({ data }) => {
-//       createCollection(data.hits)
-//       //simpleLightBox = new SimpleLightbox('.gallery a')
-//       simpleLightBox.refresh()
-
-//       const totalPages = Math.floor(data.totalHits / perPage)
-//       if (page > totalPages) {
-//         refs.btnLoad.classList.add('is-hidden') 
-//         Notify.failure("We're sorry, but you've reached the end of search results.")
-//       }
-//       scroll()
-//     })
-//     .catch(error => console.log(error))
-// } 
